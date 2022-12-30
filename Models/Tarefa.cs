3028cd9d33cf181683a00973bf16d07873dc015c
DeniseBarbosa <deniseluvoir@gmail.com>
2022-12-30 10:18:06 -0300

    Criando um arquivo chamado Tarefa na pasta Models

---
+++ Models/Tarefa.cs
@@ -0,0 +1,10 @@
+namespace Api.Models{
+    public class Tarefa{
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public bool Concluido { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+
+    }
+}
