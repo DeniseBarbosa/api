95405107b5e9c1f327e085e7ea0bb569ecee823e
DeniseBarbosa <deniseluvoir@gmail.com>
2022-12-30 15:10:53 -0300

    Atualizando e excluindo um registro

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,13 +9,22 @@ namespace Api.Controllers{
     public class HomeController : ControllerBase
     {
         [HttpGet]
-        [Route("/obter")]
+        [Route("/todaslistas")]
         public List<Tarefa> Get([FromServices] AppDbContext context){
                 return context.Tarefas.ToList();
         }
 
+        [HttpGet]
+        [Route("/{id:int}")]
+        public Tarefa GetId(
+            int id,
+            [FromServices] AppDbContext context)
+        {
+                return context.Tarefas.FirstOrDefault(x=>x.Id == id);
+        }
+
         [HttpPost]
-        [Route("/enviar")]
+        [Route("/cadastrar")]
         public Tarefa Post(
             [FromBody] Tarefa tarefa,
             [FromServices] AppDbContext context){
@@ -23,6 +32,42 @@ namespace Api.Controllers{
                 context.SaveChanges();
                 return tarefa;
         }
+
+        [HttpPut]
+        [Route("/{id:int}")]
+        public Tarefa Atualizar(
+            int id,
+            [FromBody] Tarefa tarefa,
+            [FromServices] AppDbContext context)
+        {
+                var atualizar = context.Tarefas.FirstOrDefault(x=>x.Id == id);
+                if(atualizar == null)
+                    return tarefa;
+                tarefa.Titulo = tarefa.Titulo;
+                tarefa.Concluido = tarefa.Concluido;
+
+                context.Tarefas.Update(tarefa);
+                context.SaveChanges();
+                return tarefa;
+        }
+
+
+        [HttpDelete]
+        [Route("/{id:int}")]
+        public Tarefa Excluir(
+            int id,
+            //[FromBody] Tarefa tarefa,
+            [FromServices] AppDbContext context)
+        {
+                var excluir = context.Tarefas.FirstOrDefault(x=>x.Id == id);
+            
+                context.Tarefas.Remove(excluir);
+                context.SaveChanges();
+                return excluir;
+        }
+        
+
+
     }
 
 }
